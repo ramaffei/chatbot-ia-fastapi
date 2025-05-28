4de166d12632de3891a8ec981d69e20b21e4a5c6
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2025-05-28 07:10:53 -0300

    Add message handling to ChatService and enhance ChatRepository methods

--- app/repositories/chat_repository.py
+++ app/repositories/chat_repository.py
@@ -1,5 +1,5 @@
 from exceptions.chat import ChatException
-from models.chat_model import Chat
+from models.chat_model import Chat, Message
 from repositories.base import ModelRepository
 
 
@@ -7,3 +7,19 @@ class ChatRepository(ModelRepository):
     model: Chat = Chat
     field_not_found = ChatException(ChatException.ErrorCode.Chat_Not_Found)
     already_exists_error = ChatException(ChatException.ErrorCode.Already_Exist)
+
+    def get_messages_by_chat_id(self, chat_id: str) -> list[Message]:
+        chat: Chat = self.get(chat_id)
+        if not chat:
+            raise self.field_not_found
+        return chat.messages or []
+
+    def create_message(self, chat_id: str, role: str, content: str) -> Message:
+        chat: Chat = self.get(chat_id)
+        if not chat:
+            raise self.field_not_found
+
+        message = Message(role=role, content=content, chat_id=chat.id)
+        chat.messages.append(message)
+        self.update_instance(chat)
+        return message
--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -1,8 +1,10 @@
 from langchain_core.language_models.chat_models import BaseChatModel
-from models.chat_model import Chat
+from models.chat_model import Chat, Message, MessageRole
 from providers.llm_factory import get_llm
 from repositories.chat_repository import ChatRepository
 from sqlalchemy.orm import Session
+from langchain_core import messages
+from langchain_core.messages import BaseMessage
 
 
 class ChatService:
@@ -14,12 +16,42 @@ class ChatService:
         self.username: str | None = username
         self.repository = ChatRepository(session)
 
-    def get_response_message(self, messages: list[str]) -> str:
-        response = self.llm.invoke(messages)
+    def get_response_message(self, user_message: str) -> str:
+        self.repository.create_message(
+            chat_id=self.chat_id,
+            role=MessageRole.HumanMessage,
+            content=user_message,
+        )
+        messages_history = self._build_history()
+        response = self.llm.invoke(messages_history)
+        self.repository.create_message(
+            chat_id=self.chat_id,
+            role=MessageRole.AIMessage,
+            content=response.content,
+        )
         return response.content
 
-    def _build_history(self) -> None:
-        return
+    def _build_history(self) -> list[BaseMessage]:
+        chat_messages: list[Message] = self.repository.get_messages_by_chat_id(
+            self.chat_id
+        )
+        history = []
+        history.append(self._get_system_message())
+        for message in chat_messages:
+            history.append(self._build_message(message.content, message.role.name))
+        return history
+
+    def _get_system_message(self) -> BaseMessage | None:
+        return messages.SystemMessage(
+            "Por favor, responde a las preguntas de manera clara y concisa."
+        )
+
+    def _build_message(self, content: str, role: str) -> BaseMessage:
+        try:
+            cls = getattr(messages, role)
+            return cls(content)
+        except AttributeError:
+            raise ValueError(f"No se encontró la clase '{role}' en langchain_core")
 
     @property
     def chat_id(self) -> str | None:
